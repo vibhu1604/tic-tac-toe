9dfbc4ec6277894f4c1c46f27527308ea02270a0
Vibhuti Gohil <31760633+vibhu1604@users.noreply.github.com>
2024-07-10 14:33:24 -0400

    Basic Structure of Game Board

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">0</span>
+          </li>
+        </ol>
+        Game Board
+      </div>
+      LOG
+    </main>
   )
 }
 
