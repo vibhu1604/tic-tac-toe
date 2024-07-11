3d1c44cedf7ac8480214842f43fe0d06266a2fba
Vibhuti Gohil <31760633+vibhu1604@users.noreply.github.com>
2024-07-11 19:39:45 -0400

    managed stat when clicked the square button

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,16 +1,36 @@
+import { useState } from "react";
+
 const initalGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
 ];
-export default function GameBoard(){
-    return(
-        <ol id="game-board">
-            {initalGameBoard.map((row, rowIndex) => <li key={rowIndex}>
-                <ol>
-                    {row.map((playerSymbol, colIndex) => <li key={colIndex}><button>{playerSymbol}</button></li>)}
-                </ol>
-            </li> )}
-        </ol>
-    )
+export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initalGameBoard);
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  }
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
 }
