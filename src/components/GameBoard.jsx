7d6d62a9925a6782539fbb6d4913d3fe9ff66927
Vibhuti Gohil <31760633+vibhu1604@users.noreply.github.com>
2024-08-02 18:20:16 -0400

    updated the GameBoard Component
    Now we get to know the Order of the square clicked by each players.

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,22 +1,26 @@
-import { useState } from "react";
-
 const initalGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
-export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
-  const [gameBoard, setGameBoard] = useState(initalGameBoard);
-  function handleSelectSquare(rowIndex, colIndex) {
-    setGameBoard((prevGameBoard) => {
-      const updatedBoard = [
-        ...prevGameBoard.map((innerArray) => [...innerArray]),
-      ];
-      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-      return updatedBoard;
-    });
-    onSelectSquare();
+export default function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = initalGameBoard;
+  for (const turn of turns){
+    const {square, player} = turn;
+    const {row, col} = square;
+    gameBoard[row][col] = player;
   }
+  // const [gameBoard, setGameBoard] = useState(initalGameBoard);
+  // function handleSelectSquare(rowIndex, colIndex) {
+  //   setGameBoard((prevGameBoard) => {
+  //     const updatedBoard = [
+  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
+  //     ];
+  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //     return updatedBoard;
+  //   });
+  //   onSelectSquare();
+  // }
   return (
     <ol id="game-board">
       {gameBoard.map((row, rowIndex) => (
@@ -24,7 +28,7 @@ export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
                   {playerSymbol}
                 </button>
               </li>
